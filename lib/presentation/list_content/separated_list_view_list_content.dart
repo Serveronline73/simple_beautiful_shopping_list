1432a216099030e13ac77fe2e4ffd6f7663782b6
dennis <info.dennis.durmus@gmail.com>
2024-09-20 14:03:40 +0200

    finish task

--- lib/presentation/list_content/separated_list_view_list_content.dart
+++ lib/presentation/list_content/separated_list_view_list_content.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:simple_beautiful_shopping_list/models/product.dart';
+import 'package:simple_beautiful_shopping_list/presentation/list_entry.dart';
 
 class SeparatedListViewListContent extends StatelessWidget {
   const SeparatedListViewListContent({super.key, required this.products});
-
   final List<Product> products;
-
   @override
   Widget build(BuildContext context) {
     // Hier soll eine ListView zurückgegeben werden.
-    throw UnimplementedError();
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return ListEntry(product: products[index]);
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16);
+        },
+        itemCount: products.length);
   }
 }
